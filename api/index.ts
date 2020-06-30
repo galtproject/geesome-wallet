d5f296a9c8e09e8c8b2ab049700cfe90762ac522
jonybang <Jonybange@gmail.com>
2020-06-30 04:08:58 +0200

    add session

--- api/index.ts
+++ api/index.ts
@@ -41,44 +41,44 @@ module.exports = (appService: IGAppService, port) => {
     );
     store.sync();
 
-    function setHeaders(res) {
-        res.setHeader('Access-Control-Allow-Origin', ['localhost', "app.galtproject.io"]);
+    function setHeaders(req, res) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
         res.setHeader('Access-Control-Allow-Headers', "*");
     }
 
     service.post('/v1/create-wallet', async (req, res) => {
-        setHeaders(res);
+        setHeaders(req, res);
         req.session.secret = uuidv4();
         res.send(await appService.createWallet(req.body));
     });
 
     service.post('/v1/get-crypto-metadata-by-email', async (req, res) => {
-        setHeaders(res);
+        setHeaders(req, res);
         res.send(await appService.getCryptoMetadataByEmail(req.body.email));
     });
 
     service.post('/v1/get-wallet-by-email-and-password-hash', async (req, res) => {
-        setHeaders(res);
+        setHeaders(req, res);
         req.session.secret = uuidv4();
         res.send(await appService.getWalletByEmailAndPasswordHash(req.body.email, req.body.passwordHash));
     });
 
     service.post('/v1/update-wallet', async (req, res) => {
-        setHeaders(res);
+        setHeaders(req, res);
         res.send(await appService.updateWallet(req.body.primaryAddress, req.body.signature, req.body.walletData, req.body.expiredOn));
     });
 
     service.post('/v1/get-session', async (req, res) => {
-        setHeaders(res);
+        setHeaders(req, res);
         res.send({secret: req.session.secret});
     });
 
     service.options("/*", function (req, res, next) {
-        setHeaders(res);
+        setHeaders(req, res);
         res.send(200);
     });
     service.head("/*", function (req, res, next) {
-        setHeaders(res);
+        setHeaders(req, res);
         res.send(200);
     });
     
